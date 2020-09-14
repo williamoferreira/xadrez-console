321fab3cc5ca0637046a758dd1bb85bef3684743
William Ferreira <williamoferreira@outlook.com>
2020-09-14 14:00:19 -0300

    Inicio da implementacao do tabuleiro

---
+++ xadrez-console/xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qtdeMovimentos { get; protected set; }
+        public Tabuleiro tab { get; set; }
+
+        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        {
+            this.posicao = posicao;
+            this.tab = tab;
+            this.cor = cor;
+            this.qtdeMovimentos = 0;
+        }
+    }
+}
